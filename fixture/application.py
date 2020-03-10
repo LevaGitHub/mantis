d17e806754ccdde9cb3ddec19830b94d6f77e4b3
LevaGitHub <lvov_oleg@list.ru>
2020-03-11 00:09:58 +0300

    Создан тест авторизации

---
+++ fixture/application.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+waiting_time = 1
+
+
+class Application:
+
+    def __init__(self, browser, base_ulr):
+        if browser == 'firefox':
+            self.wd = webdriver.Firefox()
+        elif browser == 'chrome':
+            self.wd = webdriver.Chrome()
+        elif browser == 'ie':
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.wd.implicitly_wait(waiting_time)
+        self.session = SessionHelper(self)
+        self.base_ulr = base_ulr
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get(self.base_ulr)
+
+    def destroy(self):
+        self.wd.quit()
+
+    def set_textbox_value(self, textbox_name, value):
+        wd = self.wd
+        if value is not None:
+            wd.find_element_by_name(textbox_name).click()
+            wd.find_element_by_name(textbox_name).clear()
+            wd.find_element_by_name(textbox_name).send_keys(value)
+
+    def select_combobox_value(self, combobox_name, value):
+        wd = self.wd
+        if value is not None:
+            wd.find_element_by_name(combobox_name).click()
+            wd.find_element_by_name(combobox_name).send_keys(value)
